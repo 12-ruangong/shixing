4a42e6f6514dd6e182ff6e503f28dafe93fe7594
12-ruangong <2331458517@qq.com>
2014-03-20 16:22:25 +0800

    first
    edf

---
+++ src/fwe.java
@@ -0,0 +1,14 @@
+
+public class fwe {
+
+	/**
+	 * @param args
+	 * @author Administrator
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("huihi");
+	}
+
+}
